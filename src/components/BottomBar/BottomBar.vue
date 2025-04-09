ad49eded8dad0bef36381b3a03aa36eb075b4e7c
jonathanx66 <jonathanbauer66@gmail.com>
2025-04-09 12:21:37 +0200

    + add bottom bar

---
+++ src/components/BottomBar/BottomBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-bottom-navigation app fixed>
+    <v-btn>
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+<script setup lang="ts"></script>
--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -1,8 +1,10 @@
 <template>
   <v-container> 
     <Content></Content>
+    <BottomBar></BottomBar>
   </v-container>
 </template>
 <script setup lang="ts">
 import Content from './Content.vue';
+import BottomBar from '../BottomBar/BottomBar.vue';
 </script>
